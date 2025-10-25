6fe00d121293d2367e1646db58110d6f8b781323
kimiscool12345 <mayotoluoke@gmail.com>
2025-10-25 20:31:02 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+# Ask the user to input a number
+n = int(input("Enter a number: "))
+
+# Check if the number is divisible by 2
+if n % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
